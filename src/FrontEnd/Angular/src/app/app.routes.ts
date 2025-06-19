2b721b4f4668420c363f53c2c09b82533f35c38e
Victor Higaki <victorhigaki@outlook.com>
2025-06-19 14:59:02 -0300

    feat: add not-found initial page

--- src/FrontEnd/Angular/src/app/app.routes.ts
+++ src/FrontEnd/Angular/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetalhesProdutoComponent } from './pages/produtos/detalhes-produto/detalhes-produto.component';
 import { ListaProdutoComponent } from './pages/produtos/lista-produto/lista-produto.component';
 
@@ -6,4 +7,5 @@ export const routes: Routes = [
     { path: 'produtos', component: ListaProdutoComponent },
     { path: 'produtos/:id', component: DetalhesProdutoComponent },
     { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent },
 ];
