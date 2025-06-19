ddfb183ddcb78ff50b34409957a286e44481c82f
Victor Higaki <victorhigaki@outlook.com>
2025-06-19 14:50:12 -0300

    feat: produto list and details routes

--- src/FrontEnd/Angular/src/app/app.routes.ts
+++ src/FrontEnd/Angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DetalhesProdutoComponent } from './pages/produtos/detalhes-produto/detalhes-produto.component';
+import { ListaProdutoComponent } from './pages/produtos/lista-produto/lista-produto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'produtos', component: ListaProdutoComponent },
+    { path: 'produtos/:id', component: DetalhesProdutoComponent },
+    { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+];
---
+++ src/FrontEnd/Angular/src/app/pages/produtos/lista-produto/lista-produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-produto',
+  imports: [],
+  templateUrl: './lista-produto.component.html',
+  styleUrl: './lista-produto.component.css'
+})
+export class ListaProdutoComponent {
+
+}
