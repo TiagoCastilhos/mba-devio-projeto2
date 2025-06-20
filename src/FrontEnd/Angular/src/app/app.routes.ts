00c21ec372c1b157dad5e947ca86452b323805c2
Tiago Castilhos <36007271+TiagoCastilhos@users.noreply.github.com>
2025-06-20 13:52:32 -0300

    Delete src/FrontEnd directory

--- src/FrontEnd/Angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-
-export const routes: Routes = [
-    { path: 'produtos', loadChildren: () => import('./pages/produtos/produtos.module').then(m => m.ProdutosModule) },
-    { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-    { path: '', redirectTo: 'produtos', pathMatch: 'full' },
-    { path: '**', component: NotFoundComponent },
-];
