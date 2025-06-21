aa3562d29aa415e51b758659332e53088700bf30
Victor Higaki <victorhigaki@outlook.com>
2025-06-21 09:40:11 -0300

    feat: base input component

--- src/Frontend/Angular/src/app/components/base-input/base-input.component.ts
+++ src/Frontend/Angular/src/app/components/base-input/base-input.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-base-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './base-input.component.html',
-  styleUrl: './base-input.component.css'
 })
-export class BaseInputComponent {
+export class BaseInputComponent implements ControlValueAccessor {
+  label = input('');
+  type = input('text');
+  protected value: any;
+  
+  private ngControl = inject(NgControl, { optional: true });
+  protected onChange?: (value: string) => {};
+  protected onTouched?: () => {};
+  protected isDisabled = false;
 
+  constructor() {
+    if (this.ngControl) this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
 }
