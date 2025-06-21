bc3b816576370398555ae1ca0d755c0da77ce45c
Victor Higaki <victorhigaki@outlook.com>
2025-06-21 09:40:54 -0300

    feat: login layout input and buttons

--- src/Frontend/Angular/src/app/pages/auth/login/login.component.ts
+++ src/Frontend/Angular/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +13,14 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+  loginForm: FormGroup<LoginForm> = this.fb.group<LoginForm>({
+    email: this.fb.nonNullable.control<string>('teste@teste.com', [Validators.required]),
+    senha:  this.fb.nonNullable.control<string>('Senha@123', [Validators.required]),
+  });
+}
 
+interface LoginForm {
+  email: FormControl<string>;
+  senha: FormControl<string>;
 }
