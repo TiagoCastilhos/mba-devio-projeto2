484f1a17f534c0d3b8d9dc43b9d708ba3e4abc5e
Gilberto <Gilberto@DESKTOP-MSF9TJN>
2025-06-24 20:56:43 -0300

    Adicionando Categoria no MVC

---
+++ src/Backend/DevXpert.Store.MVC/Controllers/CategoriasController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using DevXpert.Store.Core.Business.Interfaces.Services;
+using DevXpert.Store.Core.Business.Services.Notificador;
+using DevXpert.Store.Core.Application.App;
+using DevXpert.Store.MVC.Controllers;
+using DevXpert.Store.Core.Application.ViewModels;
+using DevXpert.Store.Core.Application.Mappings;
+using DevXpert.Store.Core.Business.Models;
+
+namespace Kruger.Marketplace.MVC.Controllers
+{
+    [Authorize]
+    [Route("categorias")]
+    public class CategoriasController(ICategoriaService categoriaService,
+                                      INotificador notificador,
+                                      IAppIdentityUser user) : MainController(notificador, user)
+    {
+        private readonly ICategoriaService _categoriaService = categoriaService;
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoriaService.BuscarTodos());
+        }
+
+        [Route("detalhes/{id:Guid}")]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            return await GetById(id);
+        }
+
+
+        [Route("novo")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("novo")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Descricao,Id")] CategoriaViewModel categoriaViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(categoriaViewModel);
+
+            if (!await _categoriaService.Adicionar(MapToEntity(categoriaViewModel)))
+            {
+                GetErrorsFromNotificador();
+
+                return View(categoriaViewModel);
+            }
+
+            await _categoriaService.Salvar();
+
+            TempData["Sucesso"] = "Categoria Cadastrada.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("editar")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            return await GetById(id);
+        }
+
+        [HttpPost("editar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Nome,Descricao,Id")] CategoriaViewModel categoriaViewModel)
+        {
+            if (id != categoriaViewModel.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(categoriaViewModel);
+
+            if (!await _categoriaService.Atualizar(MapToEntity(categoriaViewModel)))
+            {
+                GetErrorsFromNotificador();
+
+                return View(categoriaViewModel);
+            }
+
+            await _categoriaService.Salvar();
+
+            TempData["Sucesso"] = "Categoria Editada.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("excluir/{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return await GetById(id);
+        }
+
+        [HttpPost("excluir/{id:guid}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var categoriaViewModel = await GetById(id);
+
+            if (!await _categoriaService.Excluir(id))
+            {
+                GetErrorsFromNotificador();
+
+                return View();
+            }
+
+            await _categoriaService.Salvar();
+
+            TempData["Sucesso"] = "Categoria Excluída.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #region PRIVATE METHODS
+
+        private async Task<IActionResult> GetById(Guid id)
+        {
+            if (id == Guid.Empty)
+                return NotFound();
+
+            var categoria = await _categoriaService.BuscarPorId(id);
+            var categoriaViewModel = MapToViewModel(categoria);
+
+            if (categoriaViewModel is null)
+                return NotFound();
+
+            return View(categoriaViewModel);
+        }
+
+        private static CategoriaViewModel MapToViewModel(Categoria categoria) => EntityMapping.MapToCategoriaViewModel(categoria);
+
+        private static Categoria MapToEntity(CategoriaViewModel categoriaViewModel) => EntityMapping.MapToCategoria(categoriaViewModel);
+
+        private static IEnumerable<CategoriaViewModel> MapToList(IEnumerable<Categoria> categorias) => EntityMapping.MapToListCategoriaViewModel(categorias);
+
+        #endregion
+    }
+}
---
+++ src/Backend/DevXpert.Store.MVC/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using DevXpert.Store.Core.Application.App;
+using DevXpert.Store.Core.Business.Services.Notificador;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevXpert.Store.MVC.Controllers
+{
+    public abstract class MainController : Controller
+    {
+        private readonly INotificador _notificador;
+        public readonly IAppIdentityUser _user;
+        protected Guid UserId { get; set; }
+        protected string UserName { get; set; }
+
+        protected MainController(INotificador notificador, IAppIdentityUser user)
+        {
+            _notificador = notificador;
+            _user = user;
+
+            if (user.IsAuthenticated())
+            {
+                UserId = user.GetUserId();
+                UserName = user.GetUsername();
+            }
+        }
+
+        protected void GetErrorsFromNotificador()
+        {
+            if (!_notificador.TemNotificacao())
+                return;
+
+            foreach (var error in _notificador.ObterNotificacoes())
+                if (!ModelState.Values.SelectMany(e => e.Errors).Any(e => e.ErrorMessage == error.Mensagem))
+                    ModelState.AddModelError(string.Empty, error.Mensagem);
+        }
+
+        protected void NotificarErro(string errorMessage)
+        {
+            _notificador.Handle(new Notificacao(errorMessage));
+        }
+    }
+}
