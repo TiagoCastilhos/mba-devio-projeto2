353a8b2dfcfa29eb0b5d0c24996a14561dd3ede5
Victor Higaki <victorhigaki@outlook.com>
2025-07-04 11:26:54 -0300

    Feature/produto busca/v1 (#44)
    * feat: not found page
    
    * feat: products seach by parameters
    
    * fix: busca ainda trazendo o campo de pesquisa

--- src/Frontend/Angular/src/app/pages/produtos/lista-produto/lista-produto.component.ts
+++ src/Frontend/Angular/src/app/pages/produtos/lista-produto/lista-produto.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import * as regular from '@fortawesome/free-regular-svg-icons';
 import * as solid from '@fortawesome/free-solid-svg-icons';
 import { Produto } from '@models/produto.model';
 import { FavoritosService } from '@services/favoritos.service';
 import { ToasterService } from '@services/toaster.service';
+import { Subscription } from 'rxjs';
 import { ProdutosService } from '../services/produtos/produtos.service';
 
 @Component({
@@ -13,49 +13,51 @@ import { ProdutosService } from '../services/produtos/produtos.service';
   templateUrl: './lista-produto.component.html',
   styleUrl: './lista-produto.component.css',
 })
-export class ListaProdutoComponent implements OnInit {
+export class ListaProdutoComponent implements OnInit, OnDestroy {
   faStarSolid = solid.faStar;
   faStarRegular = regular.faStar;
 
   private _produtoService = inject(ProdutosService);
   private _favoritoService = inject(FavoritosService);
   private _toasterService = inject(ToasterService);
-  private _route = inject(ActivatedRoute);
-  private _router = inject(Router);
   produtos: Produto[] = [];
+  private subscription!: Subscription;
 
   ngOnInit(): void {
-    this._route.queryParams.subscribe((params) => {
-      this._produtoService.getAll(this._router.url).subscribe({
-        next: (res) => {
-          this.produtos = res.data;
-        },
-        error: (err) => this._toasterService.error(err?.error?.errors),
-      });
+    this.subscription = this._produtoService.produtos$.subscribe({
+      next: (res) => {
+        this.produtos = res;
+      }
     });
   }
 
   alternarFavorito(produto: Produto) {
     !produto.favorito
       ? this._favoritoService.adicionar(produto.id).subscribe({
-          next: (res) => {
-            if (res.success) {
-              this._toasterService.success();
-              //verificar se o usuario esta logado, se nao estiver mandar ele pra tela de login
-              //chamar a api quando pronta
-              produto.favorito = !(produto.favorito ?? false);
-            }
-          },
-          error: (err) => this._toasterService.error(),
-        })
+        next: (res) => {
+          if (res.success) {
+            this._toasterService.success();
+            //verificar se o usuario esta logado, se nao estiver mandar ele pra tela de login
+            //chamar a api quando pronta
+            produto.favorito = !(produto.favorito ?? false);
+          }
+        },
+        error: (err) => this._toasterService.error(),
+      })
       : this._favoritoService.delete(produto.id).subscribe({
-          next: (res) => {
-            if (res.success) {
-              this._toasterService.success();
-              produto.favorito = !(produto.favorito ?? false);
-            }
-          },
-          error: (err) => this._toasterService.error(err?.error?.errors),
-        });
+        next: (res) => {
+          if (res.success) {
+            this._toasterService.success();
+            produto.favorito = !(produto.favorito ?? false);
+          }
+        },
+        error: (err) => this._toasterService.error(err?.error?.errors),
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
