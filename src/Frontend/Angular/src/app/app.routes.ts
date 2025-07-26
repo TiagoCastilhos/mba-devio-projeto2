fb662994054d1c1090d2346981f5551d0b729623
Victor Higaki <victorhigaki@outlook.com>
2025-07-26 08:33:00 -0300

    fix: erro favoritos api

--- src/Frontend/Angular/src/app/app.routes.ts
+++ src/Frontend/Angular/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { FavoritosGuard } from './guards/favoritos.guard';
 
 export const routes: Routes = [
     { path: 'produtos', loadChildren: () => import('./pages/produtos/produtos.module').then(m => m.ProdutosModule) },
     { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-    { path: 'favoritos', component: FavoritosComponent },
+    { path: 'favoritos', component: FavoritosComponent, canActivate: [FavoritosGuard] },
     { path: '', redirectTo: 'produtos', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent },
 ];
---
+++ src/Frontend/Angular/src/app/guards/favoritos.guard.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { AutenticacaoService } from '@services/autenticacao.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoritosGuard implements CanActivate {
+
+  authService = inject(AutenticacaoService);
+  router = inject(Router);
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    return this.validarClaims(route);
+  }
+
+  protected validarClaims(routeAc: ActivatedRouteSnapshot): boolean {
+    if (!this.authService.obterTokenUsuario()) {
+      this.router.navigate(['/auth/login/'], { queryParams: { returnUrl: this.router.url } });
+      return false;
+    }
+
+    return true;
+  }
+}
