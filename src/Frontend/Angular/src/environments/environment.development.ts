97cbd035c164d1d9de9beefc7b28a292709451ca
Cristian Kruger <cristian.kruger@live.com>
2025-08-01 10:56:49 -0300

    fix: ajuste de path para leitura de imagem pelo FrontEnd

--- src/Frontend/Angular/src/environments/environment.development.ts
+++ src/Frontend/Angular/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:7094/api',
-    imagesBaseUrl: 'https://localhost:7094/imagens/development',
+    imagesBaseUrl: 'https://localhost:7094/imagens',
     defaultImage: '00000000-0000-0000-0000-000000000000_imagem.jpg'
 };
